6765eeee344d7d147ebcee9181a9b8e8ab122959
dmitry <wot43ak@mail.ru>
2023-04-19 08:40:09 +0300

    v1

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import telebot
 import config
 
-bot = telebot.TeleBot(config.TOKENs)
+bot = telebot.TeleBot(config.TOKEN)
+
+@bot.message_handler(content_types=['text'])
+
+def test1(message):
+    bot.send_message(message.chat.id, message.text)
+
+bot.polling(none_stop=True)
 
