e47152c750aed22604eae72f22e5f78acf23f2ea
Dmitry <wot43ak@mail.ru>
2023-04-19 14:13:41 +0300

    v5

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 import telebot
 import config
-import socket
-
 import http.client
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(content_types=['text'])
-
-
-def test1(message):
+listCommand = ['/help - список имеющихся команд', '/ip - ip компьютера']
+@bot.message_handler(commands=['ip'])
+def checkip(message):
     conn = http.client.HTTPConnection("ifconfig.me")
     conn.request("GET", "/ip")
     x = conn.getresponse().read()
-    print(x)
-    hostname = socket.gethostname()
-
-    IPAddr = socket.gethostbyname(hostname)
     bot.send_message(message.chat.id, x)
-    print(IPAddr)
 
+@bot.message_handler(commands=['help'])
+def help(message):
+    for(index, elem) in enumerate(listCommand):
+        bot.send_message(message.chat.id, listCommand[index])
 
 bot.polling(none_stop=True)
 
