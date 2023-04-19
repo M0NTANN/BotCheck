f49b19bd207e0cbe349e0761f369c06c9b86ce43
Dmitry <wot43ak@mail.ru>
2023-04-19 10:15:32 +0300

    v2

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import telebot
 import config
+import socket
 
 bot = telebot.TeleBot(config.TOKEN)
 
 @bot.message_handler(content_types=['text'])
 
+
 def test1(message):
-    bot.send_message(message.chat.id, message.text)
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
+    bot.send_message(message.chat.id, hostname, IPAddr)
 
 bot.polling(none_stop=True)
 
